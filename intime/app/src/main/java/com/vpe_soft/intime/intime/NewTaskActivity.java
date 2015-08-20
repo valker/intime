15311df1f11045e97f1665b2dfb92f61fe8cc925
valentin.pimenov <valker.rus@gmail.com>
2015-08-21 00:55:53 +0300

    add new task fragment with initial layout

--- intime/app/src/main/java/com/vpe_soft/intime/intime/NewTaskActivity.java
+++ intime/app/src/main/java/com/vpe_soft/intime/intime/NewTaskActivity.java
@@ -1,11 +1,12 @@
 package com.vpe_soft.intime.intime;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class NewTaskActivity extends AppCompatActivity {
+public class NewTaskActivity extends AppCompatActivity implements NewTaskFragment.OnFragmentInteractionListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,5 +34,10 @@ public class NewTaskActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        //todo: implement
     }
 }
