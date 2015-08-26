38facd00fa9bd70e85c190c769a3098574df4bba
valentin.pimenov <valker.rus@gmail.com>
2015-08-26 23:51:22 +0300

    notification on alarm prototype

--- intime/app/src/main/java/com/vpe_soft/intime/intime/AlarmReceiver.java
+++ intime/app/src/main/java/com/vpe_soft/intime/intime/AlarmReceiver.java
@@ -1,5 +1,7 @@
 package com.vpe_soft.intime.intime;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -13,6 +15,14 @@ public class AlarmReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         Log.d("VP", "AlarmReceiver.onReceive");
-        Toast.makeText(context, "Alarm went off", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(context, "Alarm went off", Toast.LENGTH_SHORT).show();
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Notification.Builder builder = new Notification.Builder(context);
+        builder.setTicker("Ticker");
+        builder.setContentTitle("Title");
+        builder.setContentText("Text");
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        Notification notification = builder.build();
+        notificationManager.notify(1, notification);
     }
 }
