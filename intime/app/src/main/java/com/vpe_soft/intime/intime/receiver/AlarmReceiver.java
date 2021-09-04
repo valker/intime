7b1bbf443a5020949e224d0c5472593580cc7b02
Valentin Pimenov <valker.rus@gmail.com>
2021-09-04 10:12:30 +0300

    Fix absence of notification for recent versions of Android
    create notification channel that should be used for building notification (only for recent versions of Android)
    details: https://developer.android.com/training/notify-user/build-notification

--- intime/app/src/main/java/com/vpe_soft/intime/intime/receiver/AlarmReceiver.java
+++ intime/app/src/main/java/com/vpe_soft/intime/intime/receiver/AlarmReceiver.java
@@ -1,12 +1,15 @@
 package com.vpe_soft.intime.intime.receiver;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import androidx.core.app.NotificationCompat;
+
+import android.os.Build;
 import android.util.Log;
 
 import com.vpe_soft.intime.intime.activity.MainActivity;
@@ -20,6 +23,7 @@ import com.vpe_soft.intime.intime.database.DatabaseUtil;
 public class AlarmReceiver extends BroadcastReceiver {
 
     private static final String TAG = "AlarmReceiver";
+    private static String CHANNEL_ID = "DefaultChannelId";
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -62,17 +66,42 @@ public class AlarmReceiver extends BroadcastReceiver {
     private static void showNotification(Context context, String s) {
         Log.d(TAG, "showNotification");
 
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        NotificationCompat.Builder builder;
+        NotificationManager notificationManager;
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // https://developer.android.com/training/notify-user/build-notification
+            // gets notification manager (new style), creates notification channel and notification builder
+            CharSequence name = context.getString(R.string.channel_name);
+            String description = context.getString(R.string.channel_description);
+            int importance = NotificationManager.IMPORTANCE_DEFAULT;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            notificationManager = context.getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+            builder = new NotificationCompat.Builder(context, CHANNEL_ID);
+        } else {
+            // gets notification manager (old style) and creates notification builder
+            notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            builder = new NotificationCompat.Builder(context);
+        }
+
+        Notification notification = createNotification(context, s, builder);
+        notificationManager.notify(AlarmUtil.NOTIFICATION_TAG, 1, notification);
+
+    }
+
+    private static Notification createNotification(Context context, String contentText, NotificationCompat.Builder builder) {
         builder.setContentTitle(context.getResources().getString(R.string.app_name));
-        builder.setContentText(s);
+        builder.setContentText(contentText);
         builder.setSmallIcon(R.drawable.notification_icon);
         builder.setDefaults(Notification.DEFAULT_ALL);
 
         Intent mainActIntent = new Intent(context, MainActivity.class);
         PendingIntent mainActivityIntent = PendingIntent.getActivity(context, 0, mainActIntent, 0);
         builder.setContentIntent(mainActivityIntent);
-        Notification notification = builder.build();
-        notificationManager.notify(AlarmUtil.NOTIFICATION_TAG, 1, notification);
+        return builder.build();
     }
 }
