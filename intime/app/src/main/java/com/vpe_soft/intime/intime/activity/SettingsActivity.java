533e79b164f4ad4f291df94b7f2562b085a7c11c
Valentin Pimenov <valker.rus@gmail.com>
2021-09-15 00:30:31 +0300

    learn data-binding and string.format
    https://developer.android.com/topic/libraries/data-binding
    https://docs.oracle.com/en/java/javase/11/docs/api/java.base/java/lang/String.html#format(java.lang.String,java.lang.Object...)

--- intime/app/src/main/java/com/vpe_soft/intime/intime/activity/SettingsActivity.java
+++ intime/app/src/main/java/com/vpe_soft/intime/intime/activity/SettingsActivity.java
@@ -1,6 +1,7 @@
 package com.vpe_soft.intime.intime.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.graphics.Typeface;
 import android.os.Bundle;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import com.vpe_soft.intime.intime.BuildConfig;
 import com.vpe_soft.intime.intime.R;
+import com.vpe_soft.intime.intime.databinding.ActivitySettingsBinding;
 import com.vpe_soft.intime.intime.view.ViewUtil;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -27,8 +29,10 @@ public class SettingsActivity extends AppCompatActivity {
         TextView group1item1description = findViewById(R.id.group1item1description);
         TextView group1item2title = findViewById(R.id.group1item2title);
         TextView group1item2description = findViewById(R.id.group1item2description);
-        TextView version = findViewById(R.id.version_txt);
-        version.setText("Version: " + BuildConfig.VERSION_NAME + " (" + BuildConfig.GitHash + ")");
+
+        ActivitySettingsBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_settings);
+        binding.setAppVersion(String.format(getString(R.string.version_format_string), BuildConfig.VERSION_NAME, BuildConfig.GitHash));
+
         View actionExport = findViewById(R.id.action_export);
         View actionImport = findViewById(R.id.action_import);
         actionExport.setOnClickListener(new View.OnClickListener() {
