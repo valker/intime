1252d00c5216bb5ad3ba3f8b8a8a0068758b79ac
Valentin Pimenov <valker.rus@gmail.com>
2023-02-16 22:32:12 +0300

    add button "this app in google play"

--- intime/app/src/main/java/com/vpe_soft/intime/intime/activity/SettingsActivity.java
+++ intime/app/src/main/java/com/vpe_soft/intime/intime/activity/SettingsActivity.java
@@ -1,6 +1,9 @@
 package com.vpe_soft.intime.intime.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
@@ -21,5 +24,16 @@ public class SettingsActivity extends AppCompatActivity {
         binding.setAppVersion(String.format(getString(R.string.version_format_string),
                                             BuildConfig.VERSION_NAME,
                                             BuildConfig.GitHash));
+
+        // taken from here https://stackoverflow.com/a/11753070
+        View gotoGoogleBtn = findViewById(R.id.goto_google_play_btn);
+        gotoGoogleBtn.setOnClickListener(view1 -> {
+            final String appPackageName = getPackageName();
+            try {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+            } catch (android.content.ActivityNotFoundException anfe) {
+                startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+            }
+        });
     }
 }
